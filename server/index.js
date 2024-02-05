2c3da12b53bcb0e406166d5c4717853e93901b99
Shreyas M S <shreyasmadappady20@gmail.com>
2024-02-05 22:54:40 +0530

    c

--- server/index.js
+++ server/index.js
@@ -1,13 +1,22 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
+app.use(express.json());
 
 app.get("/hello", (req, res) => {
   res.send("Hello");
 });
 
-
-
-app.listen(3000, () => {
-  console.log("App running on the port 3000");
-});
+mongoose
+  .connect(
+    "mongodb+srv://dummyuser:dummyuser@cluster0.4j2dxvz.mongodb.net/zenapp?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("App running on the port 3000");
+    });
+  })
+  .catch((e) => {
+    console.log(`${e} did not connect`);
+  });
