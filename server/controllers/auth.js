6da4667dcc92d1a6083d1aeccd31049bc36c70b0
Shreyas M S <shreyasmadappady20@gmail.com>
2024-02-06 11:22:09 +0530

    commit

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -4,25 +4,38 @@ import jwt from "jsonwebtoken";
 const SECRETKEY = "Shreyas";
 
 export const register = async (req, res) => {
-  const { username, password, email } = await req.body;
-  const userdata = await User.findOne({ email: email });
-
-  if (userdata) {
-    return res.status(400).json({ message: "User already exists" });
+  try {
+    const { username, password, email } = await req.body;
+    const userdata = await User.findOne({ email: email });
+    if (userdata) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+    const user = new User({ username, email, password });
+    const userdat = await user.save();
+    const token = jwt.sign({ email }, SECRETKEY);
+    res.status(200).json({
+      message: "Registration succesfull",
+      token,
+      id: userdat._id,
+      username,
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
-  const user = new User({ username, email, password });
-  const userdat = await user.save();
-  const token = jwt.sign({ email }, SECRETKEY);
-  res.status(200).json({
-    message: "Registration succesfull",
-    token,
-    id: userdat._id,
-    username,
-  });
 };
 
 export const login = async (req, res) => {
-  const { email, password } = req.body;
-  const data = User.findOne({ email });
-  console.log(data);
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) return res.status(404).json({ message: "User does not exist" });
+    if (password == user.password) {
+      const token = jwt.sign({ email }, SECRETKEY);
+      return res.status(200).json({
+        token,
+      });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
